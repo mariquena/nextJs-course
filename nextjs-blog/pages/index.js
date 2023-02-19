7dc1d45b097ec87fb7f5c7421ef437d03c384846
mariquena <mariquena@finalis.com>
2023-02-19 15:54:38 -0300

    initial blog in Next with external data file system

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,51 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+
+/*
+Since Static Generation happens once at build time,
+it's not suitable for data that updates frequently or changes on every user request.
+
+In cases like this, where your data is likely to change, you can use Server-side Rendering.
+*/
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+/*
+By returning allPostsData inside the props object in getStaticProps,
+the blog posts will be passed to the Home component as a prop. 
+*/
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello, I'm <b>Mariquena</b>.</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
